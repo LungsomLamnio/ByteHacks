43cc3c646dbcab78f1703f3e1f79a8893b15020b
Lungsom Lamnio <lungsomlamnio@gmail.com>
2024-11-21 23:23:18 +0530

    HomePage.jsx file is updated to display the data which was fetched from the backend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchHomeData } from "../api";
 
 const HomePage = () => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetchHomeData();
+      setData(response);
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
-      <p>This is the landing page of your platform.</p>
+      <p>{data}</p>
     </div>
   );
 };
