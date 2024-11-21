d0118f172391634dcf00e4c971099a3834f1ffad
Lungsom Lamnio <lungsomlamnio@gmail.com>
2024-11-21 23:30:04 +0530

    port 5000 changed to 5001

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchHomeData = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/");
+    const response = await axios.get("http://localhost:5001/");
     return response.data; // This should return "Welcome to the Home Page!"
   } catch (error) {
     console.error("Error fetching home data:", error);
